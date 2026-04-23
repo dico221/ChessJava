40c5a741340b10881c1d0d448d0375ff607869e6
dico221 <238628284+dico221@users.noreply.github.com>
2026-04-23 10:48:50 +0200

    Create Mouse.java

---
+++ chessgui/Mouse.java
@@ -0,0 +1,24 @@
+package chessgui;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Mouse extends MouseAdapter{
+    
+    public int x, y;
+    public boolean pressed;
+
+    public void mousePressed(MouseAdapter e){
+
+    }
+    public void mouseReleased(MouseEvent e){
+
+    }
+    public void mouseDragged(MouseEvent e){
+
+    }
+    public void mouseMoved(MouseEvent e){
+
+    }
+
+}
